51388f0065461a61374b21ee70c9d2d5b47f4b32
miracode <rascatim@gmail.com>
2014-10-10 14:22:22 -0700

    two tests passed

--- linked_list.py
+++ linked_list.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 """Implement a linked list.
 
 Should support the following methods:
@@ -9,3 +11,57 @@ remove(node)
 print()
 
 Do not use the Python list type to solve the problem."""
+
+
+class Node(object):
+
+    def __init__(self, val, ref, ref_to):
+        self.val = val
+        self.ref = ref
+        self.ref_to = ref_to
+
+
+class Linked_list(object):
+    i = 0  # First ref
+
+    def __init__(self, *args):
+        self.size = 0
+        self.llist = (self.insert(arg) for arg in args)
+        #for arg in args:
+        #   self.insert(arg)
+
+    def pop(self):
+        """Remove first node of linked list"""
+        pass
+
+    def insert(self, val):
+        """Insert a value to the head of a linked list"""
+        try:
+            head_ref = self.llist[0].ref
+        except TypeError:
+            head_ref = None
+        self.llist = (Node(val, self.i, head_ref), self.llist)
+        self.i += 1
+        self.size += 1
+
+    def size(self):
+        """Return the size of the linked list"""
+        return self.size
+
+    def search(self, val):
+        """Return index of value in linked list"""
+        i = 0
+        for x in self.linked:
+            if x == val:
+                return i
+            i += 1
+            return "Value does not exist in linked list"
+
+    def remove(self, node):
+        """Remove specified node from linked list"""
+        pass
+
+    def lprint(self):
+        """Print entire linked list as a tuple literal"""
+        # hint use a __ function
+        pass
--- test_linked_list.py
+++ test_linked_list.py
@@ -1,1 +1,27 @@
+import unittest
+import linked_list
+
+
 """ For each method in a class, implement a test verifying that it works"""
+
+
+class MyTest(unittest.TestCase):
+    def test_node(self):
+        """test that node is created"""
+        test_vals = [1, 0, None]
+        expected = (1, 0, None)
+        anode = linked_list.Node(test_vals[0], test_vals[1], test_vals[2])
+        actual = (anode.val, anode.ref, anode.ref_to)
+        self.assertEqual(expected, actual)
+
+    def test_llist_insert(self):
+        """test linked_list.insert method"""
+        test_val = 2
+        expected = (2, 0)
+        tlist = linked_list.Linked_list()
+        tlist.insert(test_val)
+        actual = (tlist.llist[0].val, tlist.llist[0].ref)
+        self.assertEquals(expected, actual)
+
+if __name__ == '__main__':
+    unittest.main()
