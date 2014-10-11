deab24f3f77ad72606b47ed2ffb4879667657c3c
miracode <rascatim@gmail.com>
2014-10-10 20:29:03 -0700

    changing it up

--- linked_list.py
+++ linked_list.py
@@ -15,9 +15,9 @@ Do not use the Python list type to solve the problem."""
 
 class Node(object):
 
-    def __init__(self, val, ref, ref_to):
+    def __init__(self, val, ref_to):
         self.val = val
-        self.ref = ref
+        #self.ref = ref
         self.ref_to = ref_to
 
 
@@ -26,7 +26,10 @@ class Linked_list(object):
 
     def __init__(self, *args):
         self.size = 0
-        self.llist = ()
+        #self.llist = ()
+        self.first_n = None
+        # reverse args since they are added in reverse
+        args = args[::-1]
         for arg in args:
             self.insert(arg)
 
@@ -36,12 +39,16 @@ class Linked_list(object):
 
     def insert(self, val):
         """Insert a value to the head of a linked list"""
-        try:
-            head_ref = self.llist[0].ref
-        except IndexError:
-            head_ref = None
-        self.llist = (Node(val, self.i, head_ref), ) + self.llist
-        self.i += 1
+        new_n = Node(val, self.first_n)
+        self.first_n = new_n
+        #try:
+        #    #head_ref = self.llist[0].ref
+        #    head_ref = self.first.ref
+        #except IndexError:
+        #    head_ref = None
+        ##self.llist = (Node(val, self.i, head_ref), ) + self.llist
+        #exec ()
+        #self.i += 1
         self.size += 1
 
     def size(self):
--- test_linked_list.py
+++ test_linked_list.py
@@ -8,40 +8,36 @@ import linked_list
 class MyTest(unittest.TestCase):
     def test_node(self):
         """test that node is created"""
-        test_vals = [1, 0, None]
-        expected = (1, 0, None)
-        anode = linked_list.Node(test_vals[0], test_vals[1], test_vals[2])
-        actual = (anode.val, anode.ref, anode.ref_to)
+        anode = linked_list.Node(1, None)
+        actual = (anode.val, anode.ref_to)
+        expected = (1, None)
         self.assertEqual(expected, actual)
 
     def test_llist_insert(self):
         """test linked_list.insert method"""
-        test_val = 2
-        expected = (2, 0)
         tlist = linked_list.Linked_list()
-        tlist.insert(test_val)
-        actual = (tlist.llist[0].val, tlist.llist[0].ref)
+        tlist.insert(2)
+        actual = (tlist.first_n.val, tlist.first_n.ref_to)
+        expected = (2, None)
         self.assertEquals(expected, actual)
 
     def test_llist_init(self):
         """test linked_list initializes to a tuple of nodes"""
         tlist = linked_list.Linked_list(1, 'a')
-        actual = (tlist.llist[0].val,
-                  tlist.llist[0].ref,
-                  tlist.llist[0].ref_to,
-                  tlist.llist[1].val,
-                  tlist.llist[1].ref,
-                  tlist.llist[1].ref_to)
-        expected = ('a', 1, 0, 1, 0, None)
+        # TODO: add another val
+        actual = tlist.first_n.val
+        expected = 1
         self.assertEquals(expected, actual)
 
     def test_size(self):
+        """test that size of linked list is correct"""
         tlist = linked_list.Linked_list(1, 'a', 100)
         expected = 3
         actual = tlist.size
         self.assertEquals(expected, actual)
 
     def test_search(self):
+        """test that search returns correct node"""
         tlist = linked_list.Linked_list(1, 3, 'a')
         s_node = tlist.search(3)
         actual = (s_node.val, s_node.ref, s_node.ref_to)
