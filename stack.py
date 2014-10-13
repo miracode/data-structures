56c712cadde244931ab77f382fbfa7b783bd28c0
miracode <rascatim@gmail.com>
2014-10-13 10:41:22 -0700

    pop implemented, stack data structure complete

--- stack.py
+++ stack.py
@@ -10,3 +10,11 @@ class Stack(object):
 
     def push(self, data):
         self.first_n = Node(data, self.first_n)
+
+    def pop(self):
+        if self.first_n is None:
+            raise IndexError('Stack is empty, cannot pop value.')
+        else:
+            x = self.first_n
+            self.first_n = self.first_n.ref_to
+            return x
--- test_stack.py
+++ test_stack.py
@@ -1,8 +1,31 @@
 from stack import Stack
+import unittest
 
 
-def test_push():
-    t_stack = Stack()
-    val = 8
-    t_stack.push(val)
-    assert t_stack.first_n.val == val
+class MyTest(unittest.TestCase):
+    def test_push(self):
+        """Test that push adds value to top of stack"""
+        t_stack = Stack()
+        val = 8
+        t_stack.push(val)
+        assert t_stack.first_n.val == val
+        val2 = 'a'
+        t_stack.push(val2)
+        assert t_stack.first_n.ref_to.val == val
+        assert t_stack.first_n.val == val2
+
+    def test_pop(self):
+        """Test that pop returns first value of stack and removes from stack"""
+        t_stack = Stack()
+        with self.assertRaises(IndexError) as context:
+            t_stack.pop()
+        self.assertEqual(context.exception.message,
+                         'Stack is empty, cannot pop value.')
+        t_stack.push(8)
+        t_stack.push('a')
+        x = t_stack.pop()
+        assert x.val == 'a'
+        assert t_stack.first_n.val == 8
+
+if __name__ == '__main__':
+    unittest.main()
