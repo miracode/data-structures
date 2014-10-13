fba050b5033f093058d998159ad30f3fa5e03786
miracode <rascatim@gmail.com>
2014-10-13 09:06:43 -0700

    push test passed

--- stack.py
+++ stack.py
@@ -1,4 +1,12 @@
-from linked_list import Linked_list
+from linked_list import Node
 
 """Create a stack data structure"""
 
+
+class Stack(object):
+
+    def __init__(self):
+        self.first_n = None
+
+    def push(self, data):
+        self.first_n = Node(data, self.first_n)
--- test_stack.py
+++ test_stack.py
@@ -1,8 +1,8 @@
-from stack import stack
+from stack import Stack
 
 
 def test_push():
-    t_stack = stack()
+    t_stack = Stack()
     val = 8
     t_stack.push(val)
-    assert t_stack.first.val == val
+    assert t_stack.first_n.val == val
