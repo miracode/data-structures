dae6d8be7ad9e8b92a78cf0f22ed25bfd6917531
miracode <rascatim@gmail.com>
2014-10-15 21:04:37 -0700

    changed 0th index to None and insert1 test passed

--- binary_heap.py
+++ binary_heap.py
@@ -2,5 +2,8 @@
 
 
 class MaxBinaryHeap:
-    def __init__(self, h_array=[]):
+    def __init__(self, h_array=[None]):
         self.h_array = h_array
+
+    def insert(self, val):
+        self.h_array = self.h_array + [val]
