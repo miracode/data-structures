6fec536de5fff3f361408a770e885d8a7853e736
miracode <rascatim@gmail.com>
2014-10-16 19:15:20 -0700

    asserted adding max to insert doesnt change tests

--- binary_heap.py
+++ binary_heap.py
@@ -1,17 +1,18 @@
 """Implementation of a Binary Heap"""
 
 
-class MaxBinaryHeap:
+class BinaryHeap:
 
-    def __init__(self, iterable=None, btype='max'):
+    def __init__(self, iterable=(), btype='max'):
         self.harray = [None]
         self.length = 0
-        if iterable is not None:
-            for val in iterable:
-                self.mh_insert(val)
+        self.iterable = iterable
         self.btype = btype
         if self.btype not in ['min', 'max']:
             raise ValueError(u"Binary Heap type must be 'max' or 'min'")
+        if self.iterable is not ():
+            for val in self.iterable:
+                self.mh_insert(val)
 
     def mh_insert(self, val):
         # insert val to end of list
@@ -20,14 +21,15 @@ class MaxBinaryHeap:
         vindex = self.harray.index(val)
         parent = vindex / 2
         # swap if not in right order
-        while self.harray[parent] < self.harray[vindex]:
-            if self.harray[parent] is None:
-                break
-            else:
-                self.harray[parent], self.harray[vindex] = \
-                    self.harray[vindex], self.harray[parent]
-                vindex = parent
-                parent = vindex / 2
+        if self.btype == 'max':
+            while self.harray[parent] < self.harray[vindex]:
+                if self.harray[parent] is None:
+                    break
+                else:
+                    self.harray[parent], self.harray[vindex] = \
+                        self.harray[vindex], self.harray[parent]
+                    vindex = parent
+                    parent = vindex / 2
         # increase length by 1
         self.length += 1
 
--- test_binary.py
+++ test_binary.py
@@ -4,18 +4,18 @@ import binary_heap
 
 class MyTest(unittest.TestCase):
     def test_init_max_heap(self):
-        tmaxheap = binary_heap.MaxBinaryHeap()
+        tmaxheap = binary_heap.BinaryHeap()
         self.assertEquals(tmaxheap.harray, [None])
 
     def test_max_heap_insert1(self):
-        tmaxheap = binary_heap.MaxBinaryHeap()
+        tmaxheap = binary_heap.BinaryHeap()
         val = 2
         tmaxheap.mh_insert(val)
         self.assertEquals(tmaxheap.harray, [None, val])
 
     def test_max_heap_insert_many(self):
         vals = [3, 7, 1, 4]
-        tmaxheap = binary_heap.MaxBinaryHeap()
+        tmaxheap = binary_heap.BinaryHeap()
         print tmaxheap.harray
         for val in vals:
             print val
@@ -26,21 +26,21 @@ class MyTest(unittest.TestCase):
 
     def test_init_max_heap_vals(self):
         tarray = (3, 7, 1, 4)
-        tmaxheap = binary_heap.MaxBinaryHeap(tarray)
+        tmaxheap = binary_heap.BinaryHeap(tarray)
         actual = tmaxheap.harray
         expected = [None, 7, 4, 1, 3]
         self.assertEquals(actual, expected)
 
     def test_pop_max_heap_val(self):
         tarray = (3, 7, 1, 4)
-        tmaxheap = binary_heap.MaxBinaryHeap(tarray)
+        tmaxheap = binary_heap.BinaryHeap(tarray)
         actual = tmaxheap.mh_pop()
         expected = 7
         self.assertEquals(actual, expected)
 
     def test_pop_max_heap_array(self):
         tarray = (3, 7, 1, 4, 5, 2, 9)
-        tmaxheap = binary_heap.MaxBinaryHeap(tarray)
+        tmaxheap = binary_heap.BinaryHeap(tarray)
         tmaxheap.mh_pop()
         actual = tmaxheap.harray
         expected = [None, 7, 5, 2, 3, 4, 1]
@@ -48,41 +48,41 @@ class MyTest(unittest.TestCase):
 
     def test_max_heap_length(self):
         tarray = (3, 7, 1, 4, 5, 2, 9)
-        tmaxheap = binary_heap.MaxBinaryHeap(tarray)
+        tmaxheap = binary_heap.BinaryHeap(tarray)
         actual = tmaxheap.length
         expected = 7
         self.assertEquals(actual, expected)
 
     def test_pop_max_heap_array2(self):
         tarray = (22, 7, 10, 45, 15, 27, 52, 12)
-        tmaxheap = binary_heap.MaxBinaryHeap(tarray)
+        tmaxheap = binary_heap.BinaryHeap(tarray)
         tmaxheap.mh_pop()
         actual = tmaxheap.harray
         expected = [None, 45, 22, 27, 12, 15, 10, 7]
         self.assertEquals(actual, expected)
 
     def test_btype_init(self):
-        theap = binary_heap.MaxBinaryHeap()
+        theap = binary_heap.BinaryHeap()
         actual = theap.btype
         expected = 'max'
         self.assertEquals(actual, expected)
 
     def test_min_max_error(self):
-        theap = binary_heap.MaxBinaryHeap()
+        theap = binary_heap.BinaryHeap()
         actual = theap.btype
         expected = 'max'
         self.assertEquals(actual, expected)
         tarray = (3, 7, 1, 4, 5, 2, 9)
-        theap2 = binary_heap.MaxBinaryHeap(tarray)
+        theap2 = binary_heap.BinaryHeap(tarray)
         actual = theap2.btype
         expected = 'max'
         self.assertEquals(actual, expected)
-        theap3 = binary_heap.MaxBinaryHeap(btype='min')
+        theap3 = binary_heap.BinaryHeap(btype='min')
         actual = (theap3.harray, theap3.btype)
         expected = ([None], 'min')
         self.assertEquals(actual, expected)
         with self.assertRaises(ValueError) as actual:
-            binary_heap.MaxBinaryHeap(btype='foo')
+            binary_heap.BinaryHeap(btype='foo')
         self.assertEquals(actual.exception.message,
                           u"Binary Heap type must be 'max' or 'min'")
 
