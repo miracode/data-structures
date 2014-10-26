5644a0c572baff0d5d0680a563606f7267bce1a3
miracode <rascatim@gmail.com>
2014-10-25 17:44:47 -0700

    first three test passes

--- test_bst.py
+++ test_bst.py
@@ -22,3 +22,17 @@ class BSTTest(unittest.TestCase):
                   test_bst.left.root, test_bst.left.left,
                   test_bst.left.right)
         self.assertEquals(expected, actual)
+
+    def test_insert_three1(self):
+        first_val = 9
+        second_val = 7
+        third_val = 12
+        test_bst = bst.BinarySearchTree()
+        test_bst.insert(first_val)
+        test_bst.insert(second_val)
+        test_bst.insert(third_val)
+        expected = (9, 7, None, None, 12, None, None)
+        actual = (test_bst.root, test_bst.left.root, test_bst.left.left,
+                  test_bst.left.right, test_bst.right.root,
+                  test_bst.right.left, test_bst.right.right)
+        self.assertEquals(expected, actual)
