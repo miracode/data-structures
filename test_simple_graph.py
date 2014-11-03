64b5b7537b18f1e8b957c4c3d327a29a3879b667
miracode <rascatim@gmail.com>
2014-11-03 15:36:49 -0800

    adding has node test

--- test_simple_graph.py
+++ test_simple_graph.py
@@ -92,6 +92,13 @@ class GraphTest(unittest.TestCase):
 in graph.")
         assert len(g.edges()) == 1
 
+    def test_has_node(self):
+        g = simple_graph.Graph()
+        new_node1 = simple_graph.Node('yes')
+        new_node2 = simple_graph.Node('no')
+        g.add_node(new_node1)
+        assert g.has_node(new_node1) is True
+        assert g.has_node(new_node2) is False
 
 if __name__ == '__main__':
     unittest.main()
