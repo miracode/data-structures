86cc38462499774715ff26a6b6a70d7dbc84d8a9
miracode <rascatim@gmail.com>
2014-11-03 14:39:00 -0800

    Node test passed

--- test_simple_graph.py
+++ test_simple_graph.py
@@ -8,3 +8,8 @@ class GraphTest(unittest.TestCase):
         g = simple_graph.Graph()
         no_nodes = g.nodes()
         assert no_nodes == []
+
+    def test_nodes(self):
+        g = simple_graph.Graph()
+        g.nodes_list = [simple_graph.Node(), simple_graph.Node()]
+        assert len(g.nodes()) == 2
