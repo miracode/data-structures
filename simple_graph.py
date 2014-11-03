ab454d4116f8117a440bd8acbceb516c82b23813
miracode <rascatim@gmail.com>
2014-11-03 14:31:15 -0800

    initializing graph files

---
+++ simple_graph.py
@@ -0,0 +1,22 @@
+
+class Node(object):
+    pass
+
+
+class Edge(object):
+    pass
+
+
+class Graph(object):
+
+    def __init__(self):
+        self.nodes = []
+        self.edges = []
+    
+    def nodes(self):
+        """Return list of all nodes in graph"""
+        pass
+
+    def edges(self):
+        """Return list of all edges in graph"""
+        pass
