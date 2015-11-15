6447f7e92d14b3779e626f25472836bbecbb80ed
miracode <rascatim@gmail.com>
2015-11-15 11:23:02 -0800

    making stack tests more uniform

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -4,32 +4,32 @@ import unittest
 
 class StackTest(unittest.TestCase):
     def test_init(self):
-        t_stack = Stack()
-        assert t_stack.top_node is None
+        stack = Stack()
+        self.assertIsNone(stack.top_node)
 
     def test_push(self):
         """Test that push adds value to top of stack"""
-        t_stack = Stack()
+        stack = Stack()
         val = 8
-        t_stack.push(val)
-        assert t_stack.top_node.data == val
+        stack.push(val)
+        self.assertEqual(stack.top_node.data, val)
         val2 = 'a'
-        t_stack.push(val2)
-        assert t_stack.top_node.next.data == val
-        assert t_stack.top_node.data == val2
+        stack.push(val2)
+        self.assertEqual(stack.top_node.next.data, val)
+        self.assertEqual(stack.top_node.data, val2)
 
     def test_pop(self):
         """Test that pop returns first value of stack and removes from stack"""
-        t_stack = Stack()
+        stack = Stack()
         with self.assertRaises(IndexError) as context:
-            t_stack.pop()
+            stack.pop()
         self.assertEqual(context.exception.message,
                          'Stack is empty, cannot pop value.')
-        t_stack.push(8)
-        t_stack.push('a')
-        x = t_stack.pop()
-        assert x.data == 'a'
-        assert t_stack.top_node.data == 8
+        stack.push(8)
+        stack.push('a')
+        x = stack.pop()
+        self.assertEqual(x.data, 'a')
+        self.assertEqual(stack.top_node.data, 8)
 
     def test_peek(self):
         stack = Stack()
@@ -39,7 +39,7 @@ class StackTest(unittest.TestCase):
                          'Stack is empty, cannot peek value.')
         stack.push('a')
         stack.push('b')
-        assert stack.peek() == 'b'
+        self.assertEqual(stack.peek(), 'b')
 
 if __name__ == '__main__':
     unittest.main()
